aad37885b0e2c662d23a115586e210772e6360c5
Acampbell <57328920+doc22940@users.noreply.github.com>
2021-01-25 16:50:32 -0500

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Cool Hand Luke'}</span>
             <br />
             {subtitle || "I'm the Unknown Developer."}
           </h1>
