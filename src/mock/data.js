80c181da021e930f93192865a080d96ffc3fafdc
Acampbell <57328920+doc22940@users.noreply.github.com>
2021-01-25 10:03:28 -0500

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Alec Campbell', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my world', // e.g: Welcome to my website
 };
 
 // HERO DATA
