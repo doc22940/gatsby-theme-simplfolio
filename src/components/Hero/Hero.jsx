2384934cb5bf5fdacf9b4cc77bd431e036e3399f
Acampbell <57328920+doc22940@users.noreply.github.com>
2021-01-26 18:53:36 -0500

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -36,7 +36,7 @@ const Header = () => {
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta || 'More nonsense'}
               </Link>
             </span>
           </p>
