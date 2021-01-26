7e65f58bea773bb4a77c0dad498677e2c696ff00
Acampbell <57328920+doc22940@users.noreply.github.com>
2021-01-26 18:55:39 -0500

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -45,7 +45,7 @@ const Projects = () => {
                       <div>
                         <p>
                           {info ||
-                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                            'Nothing here yet.  Check back when im not being lazy.'}
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
